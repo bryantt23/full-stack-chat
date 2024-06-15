67819fd96993f7743a1aeed25f58a41eea1b4176
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-06-16 03:35:04 +0700

    ## Version 3: MongoDB Integration

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const Message = require('./models/message');
+require('dotenv').config({ path: './.env' });
+
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
+const mongoDB = process.env.MONGODB_URI
+
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(mongoDB);
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get("/api/messages", (req, res) => {
-    const messages = [
-        { id: 1, name: "John", message: "Hi" },
-        { id: 2, name: "Jane", message: "Hello how are you?" },
-        { id: 3, name: "John", message: "I am hungry" }
-    ];
-    res.json(messages)
+app.get("/api/messages", async (req, res) => {
+    try {
+        const messages = await Message.find().sort({ createdAt: 1 })
+        res.json(messages)
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching messages', error })
+    }
 })
 
 app.listen(PORT, () => {
